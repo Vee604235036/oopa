2ba3f2a024bd8b71e96fe50c79709d88e92e25a3
Vee604235036 <54048826+Vee604235036@users.noreply.github.com>
2019-08-13 11:52:02 +0700

    Update multiplication.java

--- src/homework/multiplication.java
+++ src/homework/multiplication.java
@@ -2,14 +2,17 @@ package homework;
 
 public class multiplication {
     public static void main(String[] args){
-        //System.out.println("--------------------------------------Multiplication Table----------------------------------------"); 
-        //System.out.println("\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12");
-        //System.out.println("--------------------------------------------------------------------------------------------------");
+        int W = 12;
+        int h = 12;
+        System.out.println("--------------------------------------Multiplication Table----------------------------------------"); 
+        System.out.println("\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12");
+        System.out.println("--------------------------------------------------------------------------------------------------");
         int sum;
-        for(int i= 1;i<=12;i++){
-            for(int j=2;j<=12;j++){
+        for(int i= 2;i<=h;i++){
+            System.out.print(i);
+            for(int j=1;j<=W;j++){
                sum=i*j;
-               System.out.print(sum+"\t");
+               System.out.print("\t"+sum);
   
             }
             System.out.println("\n");
